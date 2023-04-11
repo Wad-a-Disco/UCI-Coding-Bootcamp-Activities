f9d156a450cc84ddf536753838b94ebd5cd05cbc
Wade Desco <descowade@gmail.com>
2023-04-10 20:20:15 -0700

    Completed Activity 06

--- 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,3 +1,4 @@
+// Define variables
 var one = 64;
 var two = "656302";
 var three = false;
@@ -5,19 +6,35 @@ var four = 64.55;
 var five = "Howdy!";
 var six;
 
-// Insert comments to explain what each console log below will log to the console
+// Logs the data type of 'one' variable (number)
 console.log(typeof one); 
+
+// Logs the data type of 'two' variable (string)
 console.log(typeof two); 
+
+// Logs the data type of 'three' variable (boolean)
 console.log(typeof three); 
+
+// Logs the data type of 'four' variable (number)
 console.log(typeof four); 
+
+// Logs the data type of 'five' variable (string)
 console.log(typeof five);
+
+// Logs the data type of 'six' variable (undefined)
 console.log(typeof six);
 
+// Reassign values for 'four', 'five', and 'six' variables
 four = "Hello!";
 five = false;
 six = 23;
 
-// Insert comments to explain what each console log below will log to the console
+// Logs the data type of 'four' variable (string)
 console.log(typeof four);
+
+// Logs the data type of 'five' variable (boolean)
 console.log(typeof five);
+
+// Logs the data type of 'six' variable (number)
 console.log(typeof six);
+
