59f0e941756e4b62121d24874d15eebbc2502ce9
Wade Desco <descowade@gmail.com>
2023-04-10 19:41:14 -0700

    Added code to script.js

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,20 @@
 // Write Your JavaScript Code Here
+// Define variables
+let personName = "Wade Desco";
+let pets = 1;
+let funFact = "I played sports in highschool";
+
+// Log introduction to console
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact + ".");
+
+// Reassign values for partner's information
+personName = "Jane";
+pets = 1;
+funFact = "I have climbed Mount Everest.";
+
+// Log introduction to console with partner's information
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact + ".");
