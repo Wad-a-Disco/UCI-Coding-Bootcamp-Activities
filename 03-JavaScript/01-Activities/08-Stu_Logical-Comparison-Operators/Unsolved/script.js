63c65de349e576ba3fbcc2c5035e8f5e9618c6dd
Wade Desco <descowade@gmail.com>
2023-04-10 21:03:40 -0700

    08 Activity changed code

--- 03-JavaScript/01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
+++ 03-JavaScript/01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
@@ -4,16 +4,15 @@ var c = 100;
 var d = c % b;
 var e = c / 2;
 
-var expression1 = (b === e);
-var expression2 = (e < d);
+var expression1 = (b == e); // changed === to == to allow for type coercion
+var expression2 = (e > d); // changed < to > to ensure that expression2 is true
 
 // Use comparison operators so all expressions below log to the console as true
-console.log(a === b);
+console.log(parseInt(a) === b);
 console.log(b !== e);
-console.log(c < b);
-console.log(d > 0);
+console.log(c > b);
+console.log(d >= 0);
 
 // Use logical operators so all expressions below log to the console as true
-console.log(expression1 && expression2);
-console.log( !expression1 || expression2);
-  
+console.log(expression1 || expression2);
+console.log(!expression1 && expression2);
