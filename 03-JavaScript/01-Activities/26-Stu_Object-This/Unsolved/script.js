27f35544fdca000525213d84030a73063aac05af
Wade Desco <descowade@gmail.com>
2023-04-13 19:50:51 -0700

    Added comment to answer questions within activity 26

--- 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,33 +1,37 @@
 // TODO: What does 'this' refer to?
-console.log(this);
+console.log(this); // 'this' refers to the global object, which is the window object in the browser and the global object in Node.js.
 
 // TODO: What does 'this' refer to?
-function helloThis() { 
-   console.log("Inside this function, this is " + this); 
-   }
+function helloThis() {
+console.log("Inside this function, this is " + this);
+}
+// 'this' inside the helloThis function will refer to the global object in non-strict mode, and undefined in strict mode.
 
-// TODO: What will this log? 
-var child = { 
-   age: 10,
-   ageTenYears: function() {
-       console.log(this.age + 10); 
-   }
+// TODO: What will this log?
+var child = {
+age: 10,
+ageTenYears: function() {
+console.log(this.age + 10);
+}
 };
+// This will log 20 because this.age inside the ageTenYears function refers to the age property of the child object.
 
-// TODO: What will this log? 
-var investor = {  
-  name: "Cash Saver",
-  investment: {
-    initialInvestment: 5000,
-    investmentGrowth: function() {
-      console.log(this.initialInvestment * 1.15)
-    }
-   }   
+// TODO: What will this log?
+var investor = {
+name: "Cash Saver",
+investment: {
+initialInvestment: 5000,
+investmentGrowth: function() {
+console.log(this.initialInvestment * 1.15)
+}
+}
 };
+// This will log 5750, which is the result of multiplying the initialInvestment property by 1.15.
 
 // TODO: After commenting, check your results!
-helloThis();
+helloThis(); // logs "Inside this function, this is [object Window]" in the browser, or "Inside this function, this is [object global]" in Node.js.
 
 // TODO: After commenting, check your results!
-child.ageTenYears();
-investor.investment.investmentGrowth();
+child.ageTenYears(); // logs 20
+
+investor.investment.investmentGrowth(); // logs 5750
