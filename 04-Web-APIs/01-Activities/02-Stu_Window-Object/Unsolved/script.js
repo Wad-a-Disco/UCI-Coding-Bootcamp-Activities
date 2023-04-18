ed7e2e87ff5b488524c1fa04792cfa1d08b95ba4
Wade Desco <descowade@gmail.com>
2023-04-17 18:58:25 -0700

    First activity solved

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,19 @@
 // TODO: What will the following line of code log?
 console.log(window);
 
+// It will log the entire global "window" object to the console. 
+
 // TODO: What will the following line of code log?
 console.log(window.document);
+
+// It will log the "document" object associated with the "window" object to the console.
 
 // TODO: What will the following line of code log?
 console.log(document.documentElement);
 
+// It will log the "HTML" element of the current document to the console.
+
 // TODO: What will the following line of code log?
 console.log(document.head);
 
+// It will log the "head" element of the current document to the console.
