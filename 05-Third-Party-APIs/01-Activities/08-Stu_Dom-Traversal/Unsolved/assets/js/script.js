2c295477a5d013c69fb706215df737e7c13e063f
Wade Desco <descowade@gmail.com>
2023-04-24 21:17:13 -0700

    Activity 08

--- 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,5 +2,7 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.find('.box').css('background-color', 'white');
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl.find('.box:empty').first().text('O');
