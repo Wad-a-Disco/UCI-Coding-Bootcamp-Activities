7a36dfdf43fca0eedcc87b8defa311337c6bd3d7
Wade Desco <descowade@gmail.com>
2023-05-01 20:46:41 -0700

    Activity 08

---
+++ 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -0,0 +1,14 @@
+var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Github Repo Issues \n----------');
+    for (var i = 0; i < data.length; i++) {
+      console.log('Issue URL: ' + data[i].html_url);
+      console.log('Username: ' + data[i].user.login);
+    }
+  });
+
