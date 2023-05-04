ed8aeb63bc6473bcef1fa39c978d4b4feae2937f
Wade Desco <descowade@gmail.com>
2023-05-03 21:14:22 -0700

    Activities

--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -1,7 +1,17 @@
+// The URL of the API endpoint we want to retrieve data from. This endpoint
+// does not exist, so it will return a 404 error.
 var badRequestUrl = 'https://api.github.com/unicorns';
+
+// The URL of the 404 page we want to redirect to if the API request fails.
 var redirectUrl = './404.html';
 
+// We make a `fetch()` request to the bad URL, and use the `then()` method to
+// handle the response. The response is passed as an argument to the callback
+// function.
 fetch(badRequestUrl).then(function (response) {
-  // Use a conditional to check the response status.
-  // If that status equals the conditional, then redirect to the 404 page.
+  // We use a conditional to check the response status. If the status equals
+  // 404, we redirect to the 404 page using `window.location.href`.
+  if (response.status === 404) {
+    window.location.href = redirectUrl;
+  }
 });
