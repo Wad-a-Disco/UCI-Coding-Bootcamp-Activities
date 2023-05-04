67a6db6fd6fc2e9edfd2c95c72b120076c5014ae
Wade Desco <descowade@gmail.com>
2023-05-03 19:44:25 -0700

    Activities

--- 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
@@ -1,17 +1,27 @@
-var userContainer = document.getElementById('users');
-var fetchButton = document.getElementById('fetch-button');
-
 function getApi() {
   var requestUrl = 'https://api.github.com/users?per_page=5';
+  var userContainer = document.getElementById('users');
+  userContainer.innerHTML = ''; // clear previous results
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // Use the console to examine the response
-      console.log(data);
-      // TODO: Loop through the data and generate your HTML
+      // Loop through the data and generate HTML for each user
+      data.forEach(function(user) {
+        var userLink = document.createElement('a');
+        userLink.href = user.html_url;
+        userLink.textContent = user.login;
+
+        var listItem = document.createElement('li');
+        listItem.appendChild(userLink);
+
+        userContainer.appendChild(listItem);
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
     });
 }
-fetchButton.addEventListener('click', getApi);
+
