7e508a5bfa6688915698fc72f1e6ad72f8499924
Wade Desco <descowade@gmail.com>
2023-05-22 21:00:42 -0700

    Answered 08 Activity

--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/filter.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/filter.js
@@ -24,9 +24,39 @@ const isPrime = num => {
 
 
 // TODO: Describe how filter is working in this example. What will the value of primeArray be? 
+/**
+In this example, the filter method is used on the originalArray to create a new array called primeArray. 
+The filter method applies the provided function (callback) to each element of the originalArray. 
+The callback function, isPrime, is defined separately..
+
+The isPrime function checks whether a number is prime or not. 
+It loops through numbers starting from 2 up to one less than the given number (num). 
+If the number is divisible by any of the numbers in that range, it returns false. 
+Otherwise, it returns true. So, isPrime acts as a condition for the filter method.
+
+The filter method creates a new array by iterating over each element of originalArray. 
+It passes each element to the isPrime function. 
+If the callback function returns true for an element (indicating that the number is prime), 
+that element is included in the resulting array.
+
+Therefore, the primeArray will contain only the prime numbers from the originalArray, which are [2, 5].
+ */
 const primeArray = originalArray.filter(isPrime);
 
 
 // TODO: Describe how filter is working in this example. What will the value of moreThan5Array be? 
+/**
+In this example, the filter method is used again on the originalArray to create a new array called moreThan5Array. 
+The filter method applies an arrow function as the callback.
+
+The arrow function (num => num > 5) acts as a condition for the filter method. It checks whether a number is greater than 5. 
+If the condition evaluates to true for an element, that element is included in the resulting array.
+
+Therefore, the moreThan5Array will contain only the numbers from originalArray that are greater than 5, which are [10].
+
+After running the code, the outputs will be as follows:
+[2, 10]
+[1, 3, 2, 5, 10]
+ */
 const moreThan5Array = originalArray.filter(num => num > 5);
 
--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
@@ -11,9 +11,21 @@ console.log(originalArray);
 
 
 // TODO: Describe how map is working in the example below. What will the value of tripledArray be?
+/**
+The map() function is used to create a new array called tripledArray by iterating over each element in the originalArray and applying 
+an arrow function to it. The arrow function multiplies each number by 3 and returns the result. The map() function collects all the 
+returned values and creates a new array with those values. Therefore, the value of tripledArray will be [3, 9, 6, 15, 30].
+ */
 const tripledArray = originalArray.map(data => data * 3);
 
 // TODO: Describe how map is working in the example below. What will the value of oddOrEven be?
+/**
+The map() function is used to create a new array called oddOrEven by iterating over each element in the originalArray and applying 
+an arrow function to it. The arrow function checks if each number is divisible by 2 (i.e., even) or not. If the number is even, the
+function returns the string 'even', and if it's odd, the function returns the string 'odd'. The map() function collects all the 
+returned values and creates a new array with those values. Therefore, the value of oddOrEven will be 
+['odd', 'odd', 'even', 'odd', 'even'].
+ */
 const oddOrEven = originalArray.map(num => {
   if (num % 2 === 0) {
     return 'even'
