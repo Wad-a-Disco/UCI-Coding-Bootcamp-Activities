7ba70bb89430885a85348f3cf0f0dfaf34a970eb
Wade Desco <descowade@gmail.com>
2023-05-22 21:28:21 -0700

    Activity 10 answered

--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,14 @@
-// music should be an object with title, artist, and album properties
+// It's done when I have created a music object in Unsolved/index.js.
 const music = {
-  // code here
- 
+  title: 'Song Title',
+  artist: 'Artist Name',
+  album: 'Album Name'
 };
 
-// Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+// It's done when I have added title, artist, and album properties to the music object.
 
-console.log(songSnippet)
+// It's done when I have used template literals inside the songSnippet variable to output data from the music object.
+const songSnippet = `Now playing "${music.title}" by ${music.artist} from the album "${music.album}"`;
 
+// It's done when I see the results in my command line after I run the index.js file.
+console.log(songSnippet);
