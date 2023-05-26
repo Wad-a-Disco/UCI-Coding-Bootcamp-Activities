e78c7b251d2d1ad556472fee7117787c9a023e01
Wade Desco <descowade@gmail.com>
2023-05-25 20:04:52 -0700

    Answered Activity 26 and Bonus Question

--- 09-NodeJS/01-Activities/26-Stu_Obj-Destructuring/Unsolved/index.js
+++ 09-NodeJS/01-Activities/26-Stu_Obj-Destructuring/Unsolved/index.js
@@ -4,7 +4,8 @@ const nodejs = {
   type: 'JavaScript runtime environment',
 };
 
-// TODO: Destructure the object 'nodejs'
+// Destructure the object 'nodejs'
+const { name, type } = nodejs;
 
 console.log(name); // <= Node.js
 console.log(type); // <= JavaScript runtime environment
@@ -26,7 +27,8 @@ const js = {
   },
 };
 
-// TODO: Destructure the nested object 'js'
+// Destructure the nested object 'js'
+const { framework1, framework2 } = js.tools.frameworks;
 
 console.log(framework1); // <= AngularJS
 console.log(framework2); // <= Vue.js
@@ -34,7 +36,30 @@ console.log(framework2); // <= Vue.js
 // 3. Arrays
 const languages = ['HTML', 'CSS', 'JavaScript'];
 
-// TODO: Destructure the array 'languages'
+// Destructure the array 'languages'
+const [markup, style, scripting] = languages;
 
 console.log(markup, style, scripting); // <= HTML CSS JavaScript
 console.log(markup); // <= HTML
+
+// Bonus Question: Answered
+/**
+ * When destructuring an object, you can assign a property to a variable with a different name using the syntax propertyName: 
+ * variableName. Here's an example:
+  const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+// Destructure the object 'person' and assign the 'firstName' property to a variable with a different name
+const { firstName: fName } = person;
+
+console.log(fName); // <= John
+
+ * 
+ * In the example above, we're destructuring the person object and assigning the value of the firstName property to a variable named 
+ * fName. Now, when we log fName, it outputs John.
+ * 
+ * By specifying propertyName: variableName during destructuring, you can assign the value of the property to a different variable 
+ * name.
+ */
