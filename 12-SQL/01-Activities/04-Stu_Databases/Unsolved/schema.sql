229c7d94e72c45f8441b704e1e46902545c9520f
Wade Desco <descowade@gmail.com>
2023-06-28 18:53:38 -0700

    MM

--- 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
+++ 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,9 @@
 -- Write code here or enter directly in MySQL shell --
+-- Deleting the existing database
+DROP DATABASE IF EXISTS sample_db;
+
+-- Creating a new database
+CREATE DATABASE books_db;
+
+-- Selecting the new database
+USE books_db;
