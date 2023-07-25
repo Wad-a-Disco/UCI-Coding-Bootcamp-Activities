83644ae7dcf7fdff6f500f5de94b22dfa911f16c
Wade Desco <descowade@gmail.com>
2023-07-24 21:21:12 -0700

    Module 17 - Activity 8

--- 17-CS/01-Activities/08-Stu_Queues/Unsolved/index.js
+++ 17-CS/01-Activities/08-Stu_Queues/Unsolved/index.js
@@ -1,6 +1,41 @@
-// create the Queue class with two methods to remove and add an element
 class Queue {
+    constructor() {
+      this.queue = [];
+    }
   
-}
-
-module.exports = Queue;
+    // Method to add an element to the queue
+    enqueue(element) {
+      this.queue.push(element);
+    }
+  
+    // Method to remove and return the first element from the queue
+    dequeue() {
+      if (this.isEmpty()) {
+        return null;
+      }
+      return this.queue.shift();
+    }
+  
+    // Method to check if the queue is empty
+    isEmpty() {
+      return this.queue.length === 0;
+    }
+  
+    // Method to get the size of the queue
+    size() {
+      return this.queue.length;
+    }
+  
+    // Method to view the first element in the queue without removing it
+    front() {
+      return this.isEmpty() ? null : this.queue[0];
+    }
+  
+    // Method to view the last element in the queue without removing it
+    rear() {
+      return this.isEmpty() ? null : this.queue[this.queue.length - 1];
+    }
+  }
+  
+  module.exports = Queue;
+  
